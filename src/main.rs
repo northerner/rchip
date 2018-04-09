a0dabe072841fc9a9d7f0bf87d42abae2f1bbd32
Daniel King <daniel.king5@nhs.net>
2018-04-09 23:52:09 +0100

    add 4 new instructions

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use std::{thread, time::Duration};
 
 fn main() {
     // Setup CPU and memory
-    let mut program_counter = 0x200;
+    let mut program_counter: u16 = 0x200;
     let mut current_opcode: u16 = 0;
     let mut index_register = 0;
     let mut stack_pointer = 0;
 
+    let mut registers: Vec<u8> = vec![0, 16];
     let mut memory: Vec<u8> = vec![0; 0x1000];
+    let mut stack: Vec<u16> = vec![0; 16];
 
     // Load rom
     let mut rom = File::open("pong.rom").expect("ROM file missing");
@@ -24,17 +26,47 @@ fn main() {
 
     // Main loop
     loop {
-        current_opcode = ((memory[program_counter] as u16) << 8) | (memory[program_counter + 1] as u16);
+        current_opcode = ((memory[program_counter as usize] as u16) << 8) | (memory[(program_counter + 1) as usize] as u16);
         println!("Program counter: {:X}", program_counter);
         println!("Index regster: {:X}", index_register);
         println!("Stack pointer: {:X}", stack_pointer);
+        println!("Stack : {:?}", stack);
         println!("Opcode: {:X}", current_opcode);
 
-        match (current_opcode & 0xF000) {
+        match current_opcode & 0xF000 {
             0xA000 => {
-                println!("ANNN: Sets I to the address NNN");
+                println!("ANNN: Sets index register to the address NNN");
                 index_register = current_opcode & 0x0FFF;
                 program_counter = program_counter + 2;
+            }
+            0x1000 => {
+                println!("1NNN: Jump to the address NNN");
+                program_counter = current_opcode & 0x0FFF;
+            }
+            0x2000 => {
+                println!("2NNN: Calls subroutine at the address NNN");
+                stack[stack_pointer as usize] = program_counter;
+                stack_pointer = stack_pointer + 1;
+                program_counter = current_opcode & 0x0FFF;
+            }
+            0x3000 => {
+                println!("3XNN: Skips the next instruction if VX equals NN");
+                if registers[((current_opcode & 0x0F00) >> 8) as usize] == ((current_opcode & 0x00FF) as u8) {
+                    program_counter = program_counter + 4;
+                } else {
+                    program_counter = program_counter + 2;
+                }
+            }
+            0x0000 => {
+                match current_opcode & 0x000F {
+                    0x0004 => {
+                        println!("8XY4: Calls subroutine at the address NNN");
+                    }
+                    _ => {
+                        println!("Unknown opcode: {:X}", current_opcode);
+                        program_counter = program_counter + 2;
+                    }
+                }
             }
             _ => {
                 println!("Unknown opcode: {:X}", current_opcode);
